24b51c748a9bf39333190b320ab924fb347bf3a2
wanderlc <webcloudy@gmail.com>
2025-01-19 04:20:56 -0300

    Trabalho Final modulo Logica de Programacao

--- src/Calcula.java
+++ src/Calcula.java
@@ -4,7 +4,6 @@ public class Calcula {
     public static void main(String[] args) {
         Scanner valor = new Scanner(System.in);
         double[] valores = new double[5];
-        double totalLiquido = 0;
 
         for (int i = 0; i < 5; i++) {
             System.out.print("Digite o " + (i + 1) + "° valor bruto: ");
@@ -20,8 +19,6 @@ public class Calcula {
             System.out.printf("Desconto INSS: R$ %.2f%n", descInss);
             System.out.printf("Desconto Imposto de Renda: R$ %.2f%n", descIr);
             System.out.printf("Valor Líquido: R$ %.2f%n", valorLiquido);
-
-            totalLiquido += valorLiquido;
         }
     }
 }
